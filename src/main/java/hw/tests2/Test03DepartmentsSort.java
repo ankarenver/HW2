de9e1ae73b6ac13add9879d5620a96d1b6a3f684
ankarenver <1996ankarjian@gmail.com>
2020-03-26 14:42:16 -0400

    HM_4 first commit

--- src/main/java/hw/tests2/Test03DepartmentsSort.java
+++ src/main/java/hw/tests2/Test03DepartmentsSort.java
@@ -49,15 +49,19 @@ public class Test03DepartmentsSort {
         }
     }
 
-    /**
-     * 1. go to https://www.amazon.com/gp/site-directory
-     * 2. verify that every main department name (indicated by blue rectangles in the picture) is
-     * present in the All departments dropdown (indicated by green rectangle in the picture)
-     */
+
     @Test
     public void test_mainDepartment(){
         driver.navigate().to("https://www.amazon.com/gp/site-directory");
+        Select allDropdown = new Select(driver.findElement(By.id("searchDropdownBox")));
+        List<WebElement> allElements = allDropdown.getOptions();
+        List<String> allElementsInString = new ArrayList<>();
+        allElements.forEach(each-> allElementsInString.add(each.getText().trim()));
 
+
+
+        List<WebElement> names = driver.findElements(By.xpath("//h2[@class='fsdDeptTitle']"));
+        names.forEach(each-> Assert.assertTrue(allElementsInString.contains(each.getText()),"some item names are not in dropdown"));
 
     }
 
